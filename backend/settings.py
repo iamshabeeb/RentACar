964ea1bfe18d30c41c60545a00f18eb93e02166e
iamshabeeb <shabeebshaz105101@gmail.com>
2022-10-19 18:24:01 +0530

    *

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-33wtb%i!k*t5e!e#vjwvkbt=sdm*+n_xpklnxpzrmyk#7gydwf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'api.Account'
 
 
