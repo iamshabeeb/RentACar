a7901292eb8376aa7b12f2576c9c09341c9cc06e
iamshabeeb <shabeebshaz105101@gmail.com>
2022-10-19 18:14:51 +0530

    DB_ added

--- backend/settings.py
+++ backend/settings.py
@@ -127,10 +127,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': config('NAME'),
-       'USER': config('USER'),
-       'PASSWORD': config('PASSWORD'),
-       'HOST': config('HOST'),
+       'NAME': config('DB_NAME'),
+       'USER': config('DB_USER'),
+       'PASSWORD': config('DB_PASSWORD'),
+       'HOST': config('DB_HOST'),
        'PORT': '5432',
    }
 }
